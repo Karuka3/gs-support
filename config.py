2e7a87a3d132e27629b8c1b3fc0a929ac810ee97
Karuka3 <kazuki.h.home@gmail.com>
2021-10-18 00:01:29 +0900

    Add WTF_CSRF_ENABLED

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
     SECRET_KEY = secrets.token_hex(16)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///gs-support-tools.db'
+    WTF_CSRF_ENABLED = True
